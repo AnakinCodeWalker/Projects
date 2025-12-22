37895d4518cb8fea0fd8cf186e82d52b35ffb4a0
AnakinCodeWalker <parag31ja@gmail.com>
2025-12-22 23:35:35 +0530

    made some tweaks in the User model

--- VideoVerse Backend/public/temp/models/user.models.js
+++ VideoVerse Backend/public/temp/models/user.models.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt'
 import dotenv from 'dotenv';
 dotenv.config()
 
 const UserSchema = new mongoose.Schema({
-    username: {
+    userName: {
         type: String,
         required: true,
         unique: true,
@@ -48,6 +49,49 @@ const UserSchema = new mongoose.Schema({
     timestamps: true,
 })
 
+//encrypt the password before saving it into the db
+//using the prehook
+UserSchema.pre("save", async function (next) {
+
+    //first checking ki password mai modification to nhi hua
+    if (!this.isModified("password"))
+        return next()
+
+    //then hashing the password and then saving it 
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+
+//a method to compare the passowrds.
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// creating the accesstoken using jwt
+
+UserSchema.methods.genrateAccessToken = function () {
+
+    return jwt.sign({
+        id: this._id   //payload , id of the current user
+    },
+        process.env.JWT_SECRET_KEY,
+        {
+            expiresIn: process.env.JWT_SECRET_KEY_EXPIRY
+        }
+    )
+
+}
+
+//genrating the refresh token
+UserSchema.methods.genrateRefreshToken = function () {
+    return jwt.sign({
+        id: this._id
+    },
+        process.env.JWT_REFRESH_TOKEN,
+        {
+            expiresIn: JWT_REFRESH_TOKEN_EXPIRY
+        })
+}
 
 const User = mongoose.model("User", UserSchema)
 export default User
