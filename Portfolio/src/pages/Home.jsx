d9c56aa7d4c84cb6c4c25f962663b158b0467e78
AnakinCodeWalker <parag31ja@gmail.com>
2025-12-26 23:21:51 +0530

    create the Routes

---
+++ Portfolio/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+function Home(){
+    return(
+        <div className="min-h-screen ">
+{/* lets you deal the toggle effect day-night */}
+
+
+
+{/* deals with bg particle effect */}
+
+
+{/* nav bar  */}
+
+{/* Hero section projects and etc */}
+
+{/* footer */}
+        
+        </div>
+    )
+}
+
+export default Home
