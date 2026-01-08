56d439396922845a1131533ab3d190f38e020ee3
AnakinCodeWalker <parag31ja@gmail.com>
2026-01-08 12:51:23 +0530

    Created the models

--- AuthSystem/public/temp/models/User.model.js
+++ AuthSystem/public/temp/models/User.model.js
@@ -1,6 +1,44 @@
 import mongoose from "mongoose";
-const UserSchema = new mongoose.model({
 
+const UserSchema = new mongoose.model({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    verificationToken: {
+        type: String,
+
+    },
+    resetPasswordToken: {
+        type: String,
+    },
+    resetPasswordExpiry: {
+        type: Date
+    }
 }, {
     timestamps: true
 })
