622b639c8dbc86d4a5053449941aa1b30fae267d
AnakinCodeWalker <parag31ja@gmail.com>
2026-01-09 01:20:15 +0530

    feat : Add apiError and apiResponse

--- AuthSystem/public/temp/utils/ApiError.js
+++ AuthSystem/public/temp/utils/ApiError.js
@@ -1,10 +1,19 @@
-// class ApiError extends Error{
+class ApiError extends Error {
 
-//     constructor(){
-// this.  =
-// this.  =
-// this.  =
-//         this.  =
-//     }
+    constructor(statuscode, message = "Something went Wrong", stack = " ", errors = []) {
+        this.errors = errors,
+            this.statuscode = statuscode
+        this.message = message,
+            this.stack = stack
+        this.data = null
 
-// }
+        // it shows where the error happened (file, line number, call chain)
+        if (stack)
+            this.stack = stack
+        else
+            Error.captureStackTrace(this, this.constructor)
+
+    }
+
+
+}
