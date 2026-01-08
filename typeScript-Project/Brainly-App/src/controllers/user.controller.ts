fd65f9dcbf1ea113612073e07c66cbe0b53e493c
AnakinCodeWalker <parag31ja@gmail.com>
2026-01-08 11:21:11 +0530

    Initalized a auth system project

--- typeScript-Project/Brainly-App/src/controllers/user.controller.ts
+++ typeScript-Project/Brainly-App/src/controllers/user.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
 import User from "../models/User.model.js";
 import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
 // provide a return type in here
+// add zod validations.
 const signup = async (req: Request, res: Response): void => {
 
     const { userName, password, email } = req.body;
-// u dont have to provide the type here  as while u were creating the model u defined it 
-    const findUser = await User.findOne({
+
+    if(!userName||!password||!email)
+        throw new ApiError(403,"All credentials are required")
+
+    // u dont have to provide the type here  as while u were creating the model u defined it     
+const findUser = await User.findOne({
         userName: userName,
         email: email
     })
@@ -14,9 +20,28 @@ const signup = async (req: Request, res: Response): void => {
     // create the ApiResponse class and ApiError class first
     if(findUser){
     console.log(`User with this credentials already exists`);
-    return new ApiError()
-        
-}
+    throw new ApiError(403,"User already Exists")
 }
 
-export default signup
+const newUser = await User.create({
+    userName,
+    email,
+    password
+})
+// then finding the created user and remove vulnerable details from it.
+const createdUser = await User.findById(newUser._id)
+ .select("-password -refreshToken")
+
+if(!createdUser)
+    throw new ApiError(500, "User not created")
+
+// returning the newly created user.
+    res.status(201).json(new ApiResponse(201, createdUser, "User created Successfully"))
+
+
+}
+
+
+export {signup,
+    signin
+}
--- typeScript-Project/Brainly-App/src/models/User.model.ts
+++ typeScript-Project/Brainly-App/src/models/User.model.ts
@@ -1,29 +1,48 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt"
 // Mongoose schema also have “types” add it later on.
 // add interfaces 
 
 const UserSchema = new mongoose.Schema({
 
-    userName:{
-        type:String,
-        required:true
+    userName: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        index: true
     },
-    password:{
-        type:String
+    password: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        trim:true
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     }
 
-},{
-    timestamps:true
+}, {
+    timestamps: true
 })
 
 // if u want to create some method u can do that here.
+// Hash the password before saving it into the db.
+// not creating an arrow function because it will not have the context of this
+UserSchema.pre("save",async function(next){ //why pass nect here ?
+
+
+    //explain this block of code
+    if(!this.isModified("password")){
+        return next()
+    }
+
+//hashing the password and putting it into db
+this.password = await bcrypt.hash(this.password,10) 
+})
 
 // add types here
-const User = mongoose.model("User",UserSchema)
-export  default User
+const User = mongoose.model("User", UserSchema)
+export default User
