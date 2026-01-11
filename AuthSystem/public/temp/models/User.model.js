e4eaa6960ffeb9f07cf2ceeaa851b8d3e55dcf33
AnakinCodeWalker <parag31ja@gmail.com>
2026-01-11 16:49:11 +0530

    made some tweaks in these files

--- AuthSystem/public/temp/models/User.model.js
+++ AuthSystem/public/temp/models/User.model.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
 dotenv.config()
 
 import bcrypt from  "bcrypt"
 
 
-const UserSchema = new mongoose.model({
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -50,16 +49,15 @@ const UserSchema = new mongoose.model({
 })
 
 // before saving the password to the db 
-// the hook need to have a next().
-UserSchema.pre("save",async function(next){
-//    if password not modified then call next() immediately
-    if (!this.ifModified("password")) 
-     return next()
+//     --->       the async hook does not need next
+UserSchema.pre("save",async function(){
+//    if password not modified then return immedieatly
+    if (!this.isModified("password")) 
+     return 
 
 // if modfied then hash. 
    this.password = await bcrypt.hash(this.password,10)
-    next()   
-
+      
   
 })
 
@@ -72,31 +70,7 @@ UserSchema.methods.isPasswordCorrect = async function (password) {
 }
 
 
-// genrating accessToken.
-UserSchema.methods.genrateAccessToken = async function () {
-const payload = {
-    id : this._id
-}
-    jwt.sign(payload,
-        process.env.JWT_SECRET_KEY,
-        {
-        expiresIn:process.env.JWT_SECRET_KEY_EXPIRY
-    })
-    
-}
-
-// genrating refreshToken.
-UserSchema.methods.genrateRefreshToken = async function () {
-    const payload = {
-    id : this._id
-}
-    jwt.sign(payload,
-        process.env.JWT_REFRESH_KEY,
-        {
-        expiresIn:process.env.JWT_REFRESH_KEY_EXPIRY
-    })
-}
 
 const User = mongoose.model("User", UserSchema)
 
-export { User }
+export  default User 
--- AuthSystem/public/temp/routes/User.route.js
+++ AuthSystem/public/temp/routes/User.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import  {registerUserController,  verifyUserController,userLoginController , userLogOutController } from "../controllers/registerUser.Controller.js";
-import userMiddleware from "../middleware/userMiddleware.js"
+import  {registerUserController,  verifyUserController,userLoginController , userLogOutController, resetPasswordController } from "../controllers/registerUser.Controller.js";
+import userMiddleware from "../middleware/User.middleware.js"
 const router =Router()
 
 // work if there is only one verb on the route.
@@ -8,6 +8,7 @@ const router =Router()
 router.post("/registerUser",registerUserController)
 router.get("/verify/:token",verifyUserController)  // this  token can be accessed from the req.params
 router.post("/login",userLoginController)
-router.post("/logout" ,userMiddleware,userLoginController)
+router.post("/reset" ,userMiddleware,resetPasswordController)
+router.post("/logout" ,userMiddleware,userLogOutController)
 
 export default  router 
