c3f4b87e1b19ca9ef0a25c51c2261e8c8e494ca4
AnakinCodeWalker <parag31ja@gmail.com>
2026-01-15 14:38:21 +0530

    made ApiResponse correct

--- typeScript-Project/Brainly-App/src/utils/ApiResponse.ts
+++ typeScript-Project/Brainly-App/src/utils/ApiResponse.ts
@@ -3,15 +3,39 @@ class ApiResponse <T = unknown> {
     // define the class variable later on initialize them.
     statusCode: number
     message: string
-    success: boolean
-     data ? : T
-    constructor(statusCode: number, message: string, success: boolean = true, data ?:T) {
+     data  : T | undefined   // if the data is present thien of T otherwise undefined. 
+    success : boolean = true 
+    constructor(statusCode: number, message: string, data ? :T) { //checking if data is present in the constructor then type t otherwise undefined
         this.statusCode = statusCode,
-            this.data=data   // we dont know kis type ka data aayega so we made it a generic and unknown
-        this.message = message,
-            this.success = success
+        this.message   = message, 
+        this.data = data   // we dont know kis type ka data aayega so we made it a generic and unknown
+
     }
 
 }
 
 export  {ApiResponse}
+
+
+/*
+class ApiResponse<T = unknown> {
+  statusCode: number
+  message: string
+  success: boolean
+  data: T | undefined
+
+  constructor(
+    statusCode: number,
+    message: string,
+    data?: T,
+    success: boolean = true
+  ) {
+    this.statusCode = statusCode
+    this.message = message
+    this.success = success
+    this.data = data
+  }
+}
+
+export { ApiResponse }
+*/
