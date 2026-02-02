58285d7a19e2eff1ff50e87e0e5e84d5fc45ca8c
AnakinCodeWalker <parag31ja@gmail.com>
2026-02-02 19:12:50 +0530

    initiallized a basic webscoket serverside

--- LiveLink/apps/ws-backend/src/index.ts
+++ LiveLink/apps/ws-backend/src/index.ts
@@ -1,2 +1,20 @@
 import { WebSocketServer } from "ws";
 
+const wss = new WebSocketServer({ port: 3000 })
+wss.on("connection", (ws) => {
+
+
+    ws.send("you are connected to server")
+
+    ws.on("message", (message) => {
+        console.log("received from client", message.toString());
+        ws.send(`You said: ${message}`);
+    })
+
+    ws.on("close",()=>{
+        console.log("disconnected from client")
+    })
+
+
+
+})
