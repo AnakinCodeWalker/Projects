bc7db37a5903bbc3e4b55b5f02cd0cdb15687dc0
AnakinCodeWalker <parag31ja@gmail.com>
2026-02-07 16:31:28 +0530

    created the last model

--- typeScript-Project/Brainly-App/Backend/src/models/Link.model.ts
+++ typeScript-Project/Brainly-App/Backend/src/models/Link.model.ts
@@ -1,18 +1,22 @@
 import mongoose from "mongoose";
+import { boolean } from "zod";
 const LinkSchema = new mongoose.Schema({
 
+    share:{
+        type:boolean
+    },
     hash:{
         type:String
     },
     userId:{
-     
-        
-
-
-
-        ref:"User"
-
-        
+    
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:true      
+    },
+    unique:{
+        type:boolean,
+        default:true,
     }
 },{
     timestamps:true
