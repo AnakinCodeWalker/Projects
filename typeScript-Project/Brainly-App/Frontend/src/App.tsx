891932a66fa242dea163539ace8d25bd6628d250
AnakinCodeWalker <parag31ja@gmail.com>
2026-02-08 01:29:43 +0530

    finished the button component

--- typeScript-Project/Brainly-App/Frontend/src/App.tsx
+++ typeScript-Project/Brainly-App/Frontend/src/App.tsx
@@ -1,15 +1,29 @@
 import './App.css'
 import Buttons from './components/ui/Buttons'
+import Card from './components/ui/Card'
 import PlusIcon from './icons/PlusIcon'
 import { ShareIcon } from './icons/ShareIcon'
 //  in components you pass the props 
 function App() {
 return   <>
+{/*  " " -- plain text
+     { } -- evaluated to js by bundler
+
+*/}
 
   <Buttons variants="primary" startItem= {<PlusIcon/>} text= "Add content"></Buttons>
-  <Buttons variants="secondary" startItem={<ShareIcon/>} text= "Add content"></Buttons>
+  <Buttons variants="secondary" startItem={<ShareIcon/>} text= "Share Brain"></Buttons>
 
-  {/* while passing a fucntion you have to pass it inside { } */}
+{/*  item start help in changing the height of the container dynamically.. */}
+<div className='flex gap-10 items-start'> 
+
+
+<Card  link = " https://www.youtube.com/watch?v=OJixIQFF-wA&list=RDOJixIQFF-wA&start_radio=1" type="youtube" title="youtube video"></Card>
+ <Card  link = " https://twitter.com/username/status/807811447862468608" type="twitter" title="twitter tweet"></Card>
+ <Card  link = " https://x.com/RoshanKrRaii/status/2019995104405254428" type="twitter" title="twitter tweet"></Card>
+
+</div>
+  {/* while passing a function you have to pass it inside { } */}
   </>
 
 
--- typeScript-Project/Brainly-App/Frontend/src/components/ui/Buttons.tsx
+++ typeScript-Project/Brainly-App/Frontend/src/components/ui/Buttons.tsx
@@ -1,7 +1,7 @@
 import type  { ReactElement } from "react"
 
 interface ButtonProps{
-variants : "primary"|"secondary",
+variants : "primary"|"secondary"|"Danger",
 text:string,    
 startItem ?:ReactElement,
 endItem ? : ReactElement,
@@ -9,16 +9,21 @@ endItem ? : ReactElement,
 
 // if the varaint is primary or secondary style will be provided accordingly.
 const variantClasses ={
-    "primary":"bg-purple-600 text-white",
-    "secondary" : "bg-purple-200 text-purple-600",
+    "primary":"bg-purple-600 text-white flex",
+    "secondary" : "bg-purple-200 text-purple-600 flex",
+    "Danger" : "bg-red-200 text-white-600 flex",
 }
 
 // as this style is common i put into this and used it for future
-const defaultStyles = "px-4 py-2 rounded-md font-light" 
+const defaultStyles = "px-4 py-2 rounded-md font-light items-center  " 
 
+
+// it will check ki object mai variants ki value kiss key se match krti hai , uski value aayegi
+
+// variantClasses[variants = primary ] --> object mai primary key value evaluate ho jayegi.
 const Buttons = ({variants,startItem,text}:ButtonProps) => {
-  return <button className={variantClasses[variants]+""+defaultStyles}>
-{startItem}
+  return <button className={variantClasses[variants] + " " + defaultStyles}>
+<div className="pr-4">{startItem}</div>
 {text}
 
     </button>
