f24b3f1dac8a2dd88d3c8d5e8ed802799e523256
AnakinCodeWalker <parag31ja@gmail.com>
2026-02-10 23:35:27 +0530

    Added params extracting logic inside the blog route

--- WriteFlow/Frontend/src/pages/Blog.tsx
+++ WriteFlow/Frontend/src/pages/Blog.tsx
@@ -1,7 +1,10 @@
-
+import {useParams} from "react-router-dom" // extracts data from params..
+// you can only call a hook inside a component or a react hook
 const Blog = () => {
-  return <>
-  Blog routes
+ const {id}= useParams() //  useParams is used to extracts the params 
+
+ return <>
+  Blog routes {id} 
   </>
 }
 
