7642348620fffa4b3b0fdf0266ce2d97e374b689
AnakinCodeWalker <parag31ja@gmail.com>
2026-02-10 23:36:07 +0530

    created an  Generic input form

--- WriteFlow/Frontend/src/components/ui/Auth.tsx
+++ WriteFlow/Frontend/src/components/ui/Auth.tsx
@@ -11,17 +11,16 @@ const Auth = () => {
       Create an account
     </div>
 
-    <div className="flex">
-      <div className="text-slate-300 ml-3 mr-3">
-      already have an account ?
-      </div> 
-      <span className=" text-black/50 underline decoration-black/50 ">
-        <Link to="/signin">Login</Link>
-    </span>
+    <div className="flex items-center">
+      <div className=" text-slate-300 ml-3 ">
+        already have an account ? <Link className=" text-black/50 underline decoration-black/50 ml-3" to="/signin">Login</Link>
+      </div>
+
     </div>
-<div>
-  <Inputform label="hi" placeHolder="hi" value=""/>
-</div>
+    <div>
+      <Inputform label="Username" placeHolder="Username" value=" " />
+      <Inputform label="password" placeHolder="password" type= {"password"} value=" " />
+    </div>
   </div>
 }
 
