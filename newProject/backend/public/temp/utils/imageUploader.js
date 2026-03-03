9d077bf885761199fcef9374ee7cdbcfa9370ac9
AnakinCodeWalker <parag31ja@gmail.com>
2026-03-03 23:15:01 +0530

    created a util regarding cloudinary

---
+++ newProject/backend/public/temp/utils/imageUploader.js
@@ -0,0 +1,13 @@
+// 3rd video time 46 minutes.
+
+import cloudinary from "cloudinary"
+import asyncHandler from "./asyncHandler.js"
+
+
+
+// every course will have a image for a specific course.
+const uploadImageToCloudinary = asyncHandler(async (file,folder,) => {
+    
+})
+
+export default uploadImageToCloudinary
