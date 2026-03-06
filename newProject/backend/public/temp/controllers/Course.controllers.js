ac5c4100e0a4587baba163d77656c532cefdaa46
AnakinCodeWalker <parag31ja@gmail.com>
2026-03-06 13:55:05 +0530

    completed the course controller and rating reviews controllers

--- newProject/backend/public/temp/controllers/Course.controllers.js
+++ newProject/backend/public/temp/controllers/Course.controllers.js
@@ -6,8 +6,8 @@ import asyncHandler from "../utils/asyncHandler.js"
 import Course from "../models/Course.model.js"
 
 
-const createCourse = asyncHandler(async (req,res) => {
-    const  {courseName,
+const createCourse = asyncHandler(async (req, res) => {
+    const { courseName,
         courseDescription,
         price,
         tag,
@@ -15,20 +15,41 @@ const createCourse = asyncHandler(async (req,res) => {
     } = req.body
 
     // add the url u will get from the req.files from cloudinary 3rd video 58 : 00
-     if(!courseName||
-        !courseDescription||
-        !price||
-        !tag||
+    if (!courseName ||
+        !courseDescription ||
+        !price ||
+        !tag ||
         !whatYouWillLearn)
-        throw new ApiError(400 , "Invlaid inputs.") 
+        throw new ApiError(400, "Invlaid inputs.")
 
-        
+
 })
 
 //  add pagination logic in this one 
 // add middleware into this one kyuki , isAdmin kyuki only he can create the course.
-const getAllCourse = asyncHandler(async (req,res) => {
+ const getAllCourse = asyncHandler(async (req, res) => {
+
+    const allCourse = await Course.find({},{
+    courseName:true,
+    courseDescription:true,
+    price:true,
+    thumbnail : true,
+    whatYouWillLearn:true,
     
+    // populate
+    instructor : true ,
+    courseContent : true,
+    ratingAndReviews:true,
+    tag : true,
+    studentsEnrolled : true
+
+})
+
+
+return res.status(200).json(new ApiResponse(200,"All courses"),{
+    "Courses" : allCourse
+})
+
 })
 
 export {
---
+++ newProject/backend/public/temp/controllers/RatingAndReviews.controllers.js
@@ -0,0 +1,28 @@
+import RatingAndReviews from "../models/RatingAndReviews.model.js";
+import Course from "../models/Course.model.js"
+import asyncHandler from "../utils/asyncHandler.js";
+
+// createRating
+const createRating = asyncHandler(async (req, res) => {
+
+})
+
+
+
+// getAverageRating
+const getAverageRating = asyncHandler(async (req, res) => {
+
+})
+
+
+// getAllRating
+const getAllRating = asyncHandler(async (req, res) => {
+
+})
+
+
+export {
+    createRating,
+    getAverageRating,
+    getAllRating
+}
