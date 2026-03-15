ffde002c5c378c2236dd7b19893c7c0ff6658adb
AnakinCodeWalker <parag31ja@gmail.com>
2026-03-15 16:59:25 +0530

    completed the contact us controller

---
+++ newProject/backend/src/controllers/ContactUs.controllers.js
@@ -0,0 +1,25 @@
+// done
+
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import mailsender from "../utils/mailSender.js";
+
+const contactUsController = asyncHandler(async (req, res) => {
+
+    const { email, ...userDetails } = req.body
+    if (!email)
+        throw new ApiError(400, "Email is required")
+
+   
+
+        await mailsender(email, "your data sent successfully", userDetails)
+        res.status(200).json(new ApiResponse(200, " mail sent Successfully", {
+            email,
+            userDetails
+        }))
+
+   
+})
+
+export default contactUsController
