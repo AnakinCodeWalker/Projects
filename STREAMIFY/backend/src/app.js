4b2fe0400a7e43084364e5445c203f11870603f9
AnakinCodeWalker <parag31ja@gmail.com>
2026-03-18 23:33:37 +0530

    added some routes in app.js

--- STREAMIFY/backend/src/app.js
+++ STREAMIFY/backend/src/app.js
@@ -1,7 +1,7 @@
 const app = express()
 
-import authRoutes from "./routes/auth.routes.js"
-
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/User.route.js"
 import errorMiddleware from "./middlewares/errorMiddleware.js"
 
 // app.use(express.static("public")) // in case u want to files data in server.
@@ -13,9 +13,9 @@ app.use(urlencoded({
 
 app.use(cors({
     origin: env.ORIGIN,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['content-Type', 'Authorization'],
-    credentials: true
+    credentials: true //allow fe to send cookies.
 }))
 
 app.use(express.static("public"))
@@ -26,6 +26,7 @@ app.use(cookieParser())
 app.use(helmet())
 
 app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/user",userRoutes)
 
 app.use(errorMiddleware)
 export default app
