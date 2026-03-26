b30c554441ba921d6b8fcc133d9c7f25e8972fc5
AnakinCodeWalker <parag31ja@gmail.com>
2026-03-26 22:35:03 +0530

    added more security on the routes

--- STREAMIFY/frontend/src/App.jsx
+++ STREAMIFY/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router"
 
 import HomePage from "./pages/HomePage"
-import SignupPage from "./pages/SignupPage"
+import SignupPage from "./pages/SignUpPage"
 import LoginPage from "./pages/LoginPage"
 import NotificationPage from "./pages/NotificationPage"
 import CallPage from "./pages/CallPage"
@@ -11,30 +11,23 @@ import NotFoundPage from "./pages/NotFoundPage"
 
 
 import { Toaster } from "react-hot-toast"
-import { useQuery } from "@tanstack/react-query"
-import { axiosInstance } from "./lib/axios"
+// import { getAuthUser } from "./lib/api"
+import useAuthUser from "./components/hooks/useAuthUser"
+
 const App = () => {
 
-  // usequery documentation...  instead of manually hitting an api we are using react query
-  const {
-    data: authData, // this is destructuring + renaming 
-    isLoading,
-    error
-  } = useQuery({
-    queryKey: ['authUser'], // what does this do ?
-    queryFn: async () => {
+  const { isLoading, authUser } = useAuthUser()
 
-      //  this function will hit the api 
-      const res = await axiosInstance.get("/api/v1/auth/me") // of this route
-      return res.data
-    }
-  })
-
-  const authUser = authData?.user //optional chaining so it does not crash
+  // const authUser = authData?.user //optional chaining so it does not crash
   //user aya from the route we are hitting us me response mai user bhej rhe h u can verify /auth/me 
+
   console.log(authUser);
   console.log(isLoading);
-  console.log(error);
+  // console.log(error);
+
+  const isAuthenticated = Boolean(authUser)
+
+  const isOnboarded = authUser?.isOnboarded
 
   return <>
     <Routes>
@@ -42,19 +35,38 @@ const App = () => {
       {/* “Protected Route” concepts */}
       {/* “We fetch the current user from backend using /auth/me. If user exists, allow access; otherwise redirect to login.” */}
 
-      <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />}> </Route>
+      <Route path="/" element={isAuthenticated && isOnboarded ?
+        (<HomePage />)
+        :
+        (<Navigate to={
+          !isAuthenticated ? ("/login")
+            : ("/onboarding")
+        } />)
+      }> </Route>
 
-      <Route path="/signup" element={(!authUser) ? <SignupPage /> : <Navigate to="/" />}> </Route>
 
-      <Route path="/login" element={(!authUser) ? <LoginPage /> : <Navigate to="/" />}> </Route>
+      <Route
+        path="/signup"
+        element={
+          !isAuthenticated ? <SignupPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          !isAuthenticated ? <LoginPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+        }
+      />
 
-      <Route path="/notification" element={authData ? <NotificationPage /> : <Navigate to="/signup" />}></Route>
+      <Route path="/notification" element={isAuthenticated ? <NotificationPage /> : <Navigate to="/signup" />}></Route>
 
-      <Route path="/call" element={authData ? <CallPage /> : <Navigate to="/login" />}></Route>
+      <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}></Route>
 
-      <Route path="/chat" element={authData ? <ChatPage /> : <Navigate to="/login" />}></Route>
+      <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />}></Route>
 
-      <Route path="/onboarding" element={authData ? <OnBoardingPage /> : <Navigate to="/login" />}></Route>
+      <Route path="/onboarding" element={isAuthenticated ? (
+        !isOnboarded ? <OnBoardingPage /> : <Navigate to="/" />
+      ) : < Navigate to="/login" />}></Route>
 
       <Route path="*" element={<NotFoundPage />}></Route>
 
@@ -68,3 +80,14 @@ const App = () => {
 }
 
 export default App
+
+
+// flow of routing 
+/*
+
+isAuthenticated -- yes -- isOnboarded -- yes --good to go 
+                yes -- isOnboarded -- no  -- navigate to onboard page
+                
+                not authenticated --navigate to login page
+                
+*/
