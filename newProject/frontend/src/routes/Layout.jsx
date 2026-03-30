1ed02391bfe9f6bfb174e745754c0a6b8dd58670
AnakinCodeWalker <parag31ja@gmail.com>
2026-03-30 23:05:12 +0530

    added layout for better routing

--- newProject/frontend/src/routes/Layout.jsx
+++ newProject/frontend/src/routes/Layout.jsx
@@ -3,6 +3,8 @@ import NavBar from '../Components/Common/NavBar'
 import Footer from "../Components/Common/Footer"
 import { Toaster } from "react-hot-toast";
 
+//  this helps in seprating the  logic that remains the same , like navbar ,  footer
+
 const Layout = () => {
     return <>
         <NavBar />
