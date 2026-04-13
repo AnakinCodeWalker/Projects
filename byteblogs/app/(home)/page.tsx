21eca2faff12a63aba8d787af325bc87a55093eb
AnakinCodeWalker <parag31ja@gmail.com>
2026-04-14 01:11:55 +0530

    added navbar

--- byteblogs/app/(home)/page.tsx
+++ byteblogs/app/(home)/page.tsx
@@ -1,9 +1,11 @@
 // import Image from "next/image";
 
+import Navbar from "@/components/home/header/navbar";
+
 export default function Home() {
   return (
-    <div className=" p-4 text-3xl text-center bg-white w-full h-full">
-      Home Page
+   <div>
+    <Navbar/>
     </div>
   )
 }
---
+++ byteblogs/components/home/header/SearchInput.tsx
@@ -0,0 +1,19 @@
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
+import React from 'react'
+
+const SearchInput = () => {
+    return (
+        <form action='' >
+            <div className='relative'>
+                <Search className='absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 ' />
+                <Input className='pl-10 w-48 border-black focus-visible:ring-0 '
+                    type='text'
+                    name='search'
+                    placeholder='search Articles ..' />
+            </div>
+        </form>
+    )
+}
+
+export default SearchInput
