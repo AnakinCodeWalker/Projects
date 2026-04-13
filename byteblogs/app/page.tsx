91bd139ee4e50f1e3487802623dc63ba978fe294
AnakinCodeWalker <parag31ja@gmail.com>
2026-04-13 20:28:10 +0530

    git init

---
+++ byteblogs/app/page.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" p-4 text-3xl text-center bg-white w-full h-full">
+      Home Page
+    </div>
+  )
+}
