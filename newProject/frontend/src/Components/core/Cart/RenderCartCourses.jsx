439ed7d3f572e9a4926adebbed67a691369a4721
AnakinCodeWalker <parag31ja@gmail.com>
2026-04-14 21:12:17 +0530

    added render cart courses page

--- newProject/frontend/src/Components/core/Cart/RenderCartCourses.jsx
+++ newProject/frontend/src/Components/core/Cart/RenderCartCourses.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import cart from "../../../slices/cartSlice"
+// import cart from "../../../slices/cartSlice"
+import { RiStarSLine } from "react-icons/ri";
+import ReactStars from 'react-stars'
 const RenderCartCourses = () => {
 
-  const {cart} = useSelector((state)=>state.cart)
+  const { cart } = useSelector((state) => state.cart)
   return (
     <div>
 
-{
-  cart.map((course , index)=>{
+      {
+        cart.map((course, index) => {
 
-  <div key={index}>
-      <div> </div>
-      <div> </div>
-</div>
+          <div key={index}>
 
-  } )
-}
+            <div>
+              <img src={course?.thumbnail} alt="course thumbnail" />
+            </div>
+
+            <div>
+              <p>{course?.courseName}</p>
+              <p>{course?.category?.name}</p>
+
+            </div>
+
+            <ReactStars
+              count={5}
+              size={20}
+              edit={false}
+              activeColor="#ffd700"
+              emptyIcon={<RiStarSLine />}
+              fullIcon={<RiStarSLine />} />
+
+
+
+          </div>
+
+
+        })
+      }
 
     </div>
   )
