ec3c041d55f48e808764e24ba40f070f49c0fcdf
AnakinCodeWalker <parag31ja@gmail.com>
2026-04-14 21:13:02 +0530

    added render total amount page

--- newProject/frontend/src/Components/core/Cart/RenderTotalAmount.jsx
+++ newProject/frontend/src/Components/core/Cart/RenderTotalAmount.jsx
@@ -1,8 +1,30 @@
+//  write the handleBuyCourse function ..
+// in here inside the button 
+
 import React from 'react'
+import { useSelector } from 'react-redux'
+import CtaButton from '../HomePage/CtaButton'
+
 
 const RenderTotalAmount = () => {
+  const { total } = useSelector((state) => state.cart)
+
+  function handleBuyCourse() {
+
+  }
   return (
-    <div>RenderTotalAmount</div>
+    <div>
+      <p>Total : </p>
+      <p>Rs {total} </p>
+
+      <button onClick={handleBuyCourse}>
+
+        <CtaButton active={true}
+        >
+          buy now
+        </CtaButton>
+      </button>
+    </div>
   )
 }
 
