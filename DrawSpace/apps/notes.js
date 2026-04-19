9c0991dbf77319d95bd404fdc015779e19c83de0
AnakinCodeWalker <parag31ja@gmail.com>
2026-04-19 23:35:20 +0530

    finished the http-backend

---
+++ DrawSpace/apps/notes.js
@@ -0,0 +1,21 @@
+/*
+
+declaration : true --> lets u generate , index.d.ts dusre files mai jisme v isko import kroge ts internally they search index.d.ts 
+
+
+
+include ['src']  --> sirf src files compile issey pta chlta hai kon si files compile krni hai 
+
+*/
+
+/*
+package .json mai  
+jb v  koi import kre to batata hai 
+node ko  js file yaha se load kro -- index.js
+ts ko ki types yaha se lo --  index.d.ts
+*/
+
+/*
+generate	client update karega
+db push	DB me table create karega
+*/
--- VideoVerse Backend/notes.js
+++ VideoVerse Backend/notes.js
@@ -6,3 +6,47 @@
 
 //  .select to remove some details from the  mongo db document
 //  use .select(" ") --> inside single string put the fields name without seprating them via comma.
+
+
+/*
+
+model User {
+
+  id       String  @id @default(uuid())
+  email    String
+  password String
+  name     String
+  photo    String?
+
+  rooms Room[]
+  chats Chat[]
+
+}
+
+model Room {
+
+  id        Int      @id @default(autoincrement())
+  slug      String   @unique
+  createdAt DateTime @default(now())
+ 
+  adminId   String
+
+  chats      Chat[] 
+  admin     User     @relation(references: [id], fields: [adminId])
+
+}
+
+model Chat {
+ 
+  id      Int    @id @default(autoincrement())
+  message String
+ 
+  userId  String
+  roomId  Int
+ 
+  user    User   @relation(references: [id], fields: [userId])
+  room    Room   @relation(references: [id], fields: [roomId])
+
+}
+
+*/
