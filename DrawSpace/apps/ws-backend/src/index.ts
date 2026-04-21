8195fbbbf1c50f082710d5fa83d68798474a8858
AnakinCodeWalker <parag31ja@gmail.com>
2026-04-22 03:52:47 +0530

    completed the ws backend

--- DrawSpace/apps/ws-backend/src/index.ts
+++ DrawSpace/apps/ws-backend/src/index.ts
@@ -1,10 +1,38 @@
-//  move this into a constants file and extract it on both the backends 
 
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import { env } from "./config/env.config";
 
+// in future use a map to iterate on the ws 
+
 const wss = new WebSocketServer({ port: 8080 })
+
+interface User {
+    ws: WebSocket
+    rooms: string[]
+    userId: string
+}
+
+const users: User[] = []
+
+function checkUser(token: string): null | string {
+
+    //the result should be jwtpayload after u verify 
+    try {
+        const decoded = jwt.verify(token, env.JWT_SECRET_KEY) as JwtPayload
+
+        if (!decoded || !decoded.userId)
+            return null
+
+        return decoded.userId
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(error);
+            return null
+        }else
+            return null
+    }
+}
 
 wss.on('connection', (ws, request) => {
 
@@ -12,12 +40,13 @@ wss.on('connection', (ws, request) => {
 
     if (!url)
         return
+
     // extracting the token from the params 
     const queryParams = new URLSearchParams(url.split('?')[1])
 
     const token = queryParams.get("token")
 
-    const JWT_SECRET_KEY = env.JWT_SECRET_KEY
+
 
 
     if (!token) {
@@ -26,16 +55,52 @@ wss.on('connection', (ws, request) => {
         return
     }
 
-//     the result should be jwtpayload after u verify 
-    const decoded = jwt.verify(token, JWT_SECRET_KEY) as JwtPayload
+    const userId = checkUser(token)
 
-   
-
-    if (!decoded || !decoded.userId) {
+    if (!userId) {
         ws.close()
         return
     }
-    ws.on("message", () => {
-        ws.send("hi")
+
+    users.push({
+        userId,
+        rooms: [],
+        ws
+    })
+
+    ws.on("message", (data) => {
+        // lets  u convert string object into normal js  object  
+        const parsedData = JSON.parse(data as unknown as string)
+
+        if (parsedData.type == "join_room") {
+            const currentUser = users.find(x => x.ws === ws)  // find the user in the global user array
+            currentUser?.rooms.push(parsedData.roomId) // push the id into user room  room  [ roomid ]
+        }
+
+        if (parsedData.type == "leave_room") {
+            const currentUser = users.find((x) => x.ws === ws)
+
+            if (currentUser)
+                currentUser.rooms = currentUser.rooms.filter((room) => room !== parsedData.roomId)
+
+        }
+
+        if (parsedData.type === "chat") {
+            const roomId = parsedData.roomId;
+            const message = parsedData.message
+
+            users.forEach(user => {
+                if (user.rooms.includes(roomId)) {
+
+                    user.ws.send(JSON.stringify({
+                        type: "chat",
+                        message: message,
+                        roomId
+                    }))
+                }
+            });
+        }
+
+     
     })
 })
