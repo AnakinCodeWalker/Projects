dd4e692090bed7f81859ef60126836eaef0e6707
AnakinCodeWalker <parag31ja@gmail.com>
2026-04-29 23:16:20 +0530

    final bug fix

--- STREAMIFY/frontend/src/App.jsx
+++ STREAMIFY/frontend/src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
 
       <Route path="/chat/:id" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />}></Route>
 
-      <Route path="/onboarding" element={isAuthenticated && isOnboarded ? <Navigate to="/"/> : <OnBoardingPage />} ></Route>
+      <Route path="/onboarding" element={!isAuthenticated  ? <Navigate to="/login"/> : isOnboarded ? <Navigate to="/"/> :  <OnBoardingPage />} ></Route>
 
       <Route path="*" element={<NotFoundPage />}></Route>
 
