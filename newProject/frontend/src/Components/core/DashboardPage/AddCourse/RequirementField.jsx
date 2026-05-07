e74834f15a6b7e9bed36a0afff1c57769be94535
AnakinCodeWalker <parag31ja@gmail.com>
2026-05-07 20:39:50 +0530

    added course requirement component

---
+++ newProject/frontend/src/Components/core/DashboardPage/AddCourse/RequirementField.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const RequirementField = ({name,label , register , errors, setValue ,getValues}) => {
+  
+  const [requirement ,setRequirement ] = useState("")
+  const [requirementList ,setRequirementList] = useState([])
+
+  const handleRequirement = () =>{
+if(requirement){
+    setRequirementList([...requirementList , requirement])
+setRequirement("")
+}
+  }
+  const removeRequirement = (index)=>{
+const updatedRequirementList = [...requirementList]
+updatedRequirementList.splice(index,1)
+setRequirementList(updatedRequirementList)  
+}
+    return (
+    
+    <div>
+
+    </div>
+  
+)
+}
+
+export default RequirementField
